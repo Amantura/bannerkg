501440669670db57606f5f23054ea09540d65308
Amantur <38719936+Amantura@users.noreply.github.com>
2023-12-18 13:48:10 +0600

    port

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  server: {
+    port: 3001
+  },
   integrations: [
     vue({ appEntrypoint: '/src/pages/_app.js' }),
     tailwind(
